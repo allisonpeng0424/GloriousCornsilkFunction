b032fb718eeb43956c1e76b31f09de693ab28c18
allisonpeng0424 <allisonpp@qq.com>
2020-11-22 14:26:20 +0000

    ContactUs page

---
+++ js/ContactUs.js
@@ -0,0 +1,14 @@
+window.onload=function () {
+  function El(s) {
+    return document.querySelector('#'+s)
+  }
+
+  El('Submit').addEventListener('click',function () {
+    this.innerText='Succeeded';
+    this.className='btn s';
+    El('tip').className='show'
+  })
+  El('upbtn').addEventListener('click',function () {
+    El('file').click()
+  })
+}
